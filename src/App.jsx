55dc9de190ac8ae2b84a00ba206be9aac5b7cf3a
Deepanshumelkani77 <deepumelkani1232@gmail.com>
2024-11-07 19:00:21 -0800

    setup router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import Navbar from './components/navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/home/Home.jsx'
+import Cart from './pages/cart/Cart.jsx'
+import Placeorder from "./pages/placeorder/Placeorder.jsx"
 
 const App = () => {
   return (
-    <div>
-      
+    <div className='app'>
+      <Navbar/>
+      <Routes>
+
+<Route path='/' element={<Home/>}></Route>
+<Route path='/cart' element={<Cart/>}></Route>
+<Route path='/placeholder' element={<Placeorder/>}></Route>
+      </Routes>
+
     </div>
   )
 }
