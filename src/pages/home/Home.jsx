33db4a441616313aa635bab155833b299a8d3d47
Deepanshumelkani77 <deepumelkani123@gmail.com>
2024-11-08 16:31:06 -0800

    add header folder

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import './Home.css'
+import Header from '../../components/header/Header'
 const Home = () => {
   return (
     <div>
       
+<Header/>
+
     </div>
   )
 }
