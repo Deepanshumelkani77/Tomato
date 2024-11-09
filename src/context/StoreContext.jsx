ed55a969d081499157bf523d2d4b46f97de1eb92
Deepanshumelkani77 <deepumelkani123@gmail.com>
2024-11-08 17:43:48 -0800

    add context folder

---
+++ src/context/StoreContext.jsx
@@ -0,0 +1,19 @@
+import { createContext } from "react";
+
+export const StoreContext=createContext(null);
+import { food_list } from "../assets/assets";
+
+const StoreContextProvider=(props)=>{
+    const contextValue={
+
+food_list
+
+    }
+    return(
+        <StoreContextProvider value={contextValue}>
+            {props.children}
+        </StoreContextProvider>
+    )
+}
+
+export default StoreContextProvider;
