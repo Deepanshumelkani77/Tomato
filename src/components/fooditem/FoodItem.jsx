3bece669dca7902f61277bcf8b5693600b3184a2
Deepanshumelkani77 <deepumelkani123@gmail.com>
2024-11-23 18:46:12 -0800

    add + or - button in foodlist

--- src/components/fooditem/FoodItem.jsx
+++ src/components/fooditem/FoodItem.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Fooditem.css"
 import { assets } from '../../assets/assets'
 
 const FoodItem = ({id,name,description,price,image}) => {
+
+const [itemCount,setitemCount]=useState(0);
+
   return (
     <div className='food-item'>
       
-<div className="food-item-img-continer">
+<div className="food-item-img-container">
     <img className='food-item-image'  src={image} alt="" />
+    {
+        !itemCount //thats mean if itemcounter ==0
+        ?<img className='add' onClick={(prev)=>setitemCount(prev=>+1)} src={assets.add_icon_white} alt></img>
+        :<div className='food-item-counter'>   
+        <img onClick={()=>setitemCount(prev=>prev-1)} src={assets.remove_icon_red}></img>
+        <p>{itemCount}</p>
+        <img onClick={()=>setitemCount(prev=>prev+1)} src={assets.add_icon_green} alt="" />
+                 </div>
+    }
 </div>
 
 <div className="food-item-info">
